48b919b50a721584dca4e16a12055b6eb884aa0e
Volodymyr Buzanov <vbuzanov@ukr.net>
2023-09-18 23:31:45 +0300

    task5

--- src/route/index.js
+++ src/route/index.js
@@ -67,10 +67,12 @@ router.get('/logout', function (req, res) {
 
 // Підключіть файли роутів
 const auth = require('./auth')
+const user = require('./user')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
 router.use('/', auth)
+router.use('/', user)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
